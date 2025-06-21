6416cba7291ef2130228237b3b92f4af15045eb5
OsokaVetren <WWolfskin@yandex.ru>
2025-06-21 06:29:51 +0300

    Update lobby.go

--- server/pkg/lobby.go
+++ server/pkg/lobby.go
@@ -2,22 +2,49 @@ package pkg
 
 import "time"
 
-type LobbyID int
+type LoсalId int
 
 type Lobby struct {
 	id       LobbyID
 	host     PeerID
-	members  map[PeerID]*Peer
+	members  map[LocalID]*Peer
+	global2local map[PeerId]LocalId
+	local2global map[LocalId]PeerId
 	sealedAt time.Time
 }
 
 func NewLobby(host *Peer) *Lobby {
-	host.id = 1;
 	L := &Lobby{
 		id:      LobbyID(NewID()),
-		host:    host.id,
-		members: make(map[PeerID]*Peer),
+		hostGlobal:    host.id,
+		members:      make(map[LocalID]*Peer),
+    		global2local: make(map[PeerID]LocalID),
+    		local2global: make(map[LocalID]PeerID)
 	}
-	L.members[host.id] = host;
+	L.members[1] = host;
+	L.global2local[host.id] = 1
+	L.local2global[1] = host.id
 	return L;
+}
+
+func (L *Lobby) AddMember(p *Peer) LocalID {
+  var next LocalID = 2
+  for {
+    if _, ok := L.members[next]; !ok {
+      break
+    }
+    next++
+  }
+  L.members[next] = p
+  L.global2local[p.id] = next
+  L.local2global[next] = p.id
+  return next
+}
+
+func (L *Lobby) LocalID(p *Peer) LocalID {
+	return L.global2local[p.id] 
+}
+
+func (L *Lobby) PeerByLocal(id LocalID) *Peer {
+	return L.members[id] 
 }
